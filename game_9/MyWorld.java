65dee06b744628db4952626cee7cb80481b27238
20531 <20531@hakodate-ct.ac.jp>
2022-12-22 09:50:52 +0900

    ランダム表示微調整

--- game_9/MyWorld.java
+++ game_9/MyWorld.java
@@ -19,8 +19,8 @@ public class MyWorld extends World
         super(800, 800, 1);
         addObject( new PC1(), 400, 400 );
         for (int i=0; i<1; ++i){
-             int A = 45;
-             int B = 60;
+             int A = 350;
+             int B = 450;
              int range = B - A;
              int x = A + (int)(Math.random()*(range));
              A = 0;
@@ -28,17 +28,27 @@ public class MyWorld extends World
              int y = A + (int)(Math.random()*(range));
             addObject( new ichigo(), x, y);
         }
+          for (int i=0; i<1; ++i){
+             int A = 350;
+             int B = 450;
+             int range = B - A;
+             int x = A + (int)(Math.random()*(range));
+             A = 700;
+             B = 800;
+             int y = A + (int)(Math.random()*(range));
+            addObject( new ichigo(), x, y);
+        }
         for (int i=0; i<1; ++i){
              int A = 0;
-             int B = 800;
+             int B = 100;
              int x = A + (int)(Math.random()*((B-A)+1));
-             A = 0;
+             A = 350;
              B = 450;
              int y = A + (int)(Math.random()*((B-A)+1));
             addObject( new danmaku(), x, y);
         }
 
-        addObject( new ichigo(), 400 , 0  );
+       
 
     }
 }
